1dac329e5f056e98dca8c377c8b9daea2a4bd916
root <root@antde1c.mylabserver.com>
2020-02-17 08:58:04 +0000

    Added config class to manage configuration files

---
+++ apache/manifests/params.pp
@@ -0,0 +1,17 @@
+# @summary A short summary of the purpose of this class
+#
+# Operating system-related variables (and others) 
+#
+# @example
+#   include apache::params
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
